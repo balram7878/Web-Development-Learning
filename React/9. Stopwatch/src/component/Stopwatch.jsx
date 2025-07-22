b7f3b40859f1be519ab045ac63b5a371e78a0e2b
Balram Meena <bmeena97562@gmail.com>
2025-07-22 17:32:55 +0530

    Update web development Learning

--- React/9. Stopwatch/src/component/Stopwatch.jsx
+++ React/9. Stopwatch/src/component/Stopwatch.jsx
@@ -29,34 +29,45 @@ const seconds=second%60;
   return (
     <>
       <div className={styles.stopwatch}>
+
         <div className={styles.watch}>
+
           <div className={styles.first}>
             <h1>{String(hours).padStart(2, '0')}</h1>
           </div>
+
           <div className={styles.second}>
             <h1>{String(minutes).padStart(2, '0')}</h1>
           </div>
+
           <div className={styles.third}>
             <h1>{String(seconds).padStart(2, '0')}</h1>
           </div>
+
         </div>
+
         <div className={styles.btn}>
+
           <button
             className={`${styles.start} ${styles.b}`}
             onClick={handleStart}
           >
             Start
           </button>
+
           <button className={`${styles.stop} ${styles.b}`} onClick={handleStop}>
             Stop
           </button>
+
           <button
             className={`${styles.reset} ${styles.b}`}
             onClick={handleReset}
           >
             Reset
           </button>
+          
         </div>
+
       </div>
     </>
   );
