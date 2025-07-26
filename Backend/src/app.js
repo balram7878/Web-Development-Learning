dd965cefd53cba2d1922b07bd2ee64cd9d404a82
Balram Meena <bmeena97562@gmail.com>
2025-07-26 07:41:05 +0530

    patch and put

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -6,12 +6,12 @@ app.use(express.json());
 
 const books = [
   {
-    id:1,
+    id: 1,
     title: "Rich Dad Poor Dad",
     author: "Robert Kiyosaki",
   },
   {
-    id:2,
+    id: 2,
     title: "Atomic Habits",
     author: "James Clear",
   },
@@ -24,9 +24,7 @@ app.get("/book", (req, res) => {
 
 app.get("/book/:id", (req, res) => {
   const id = parseInt(req.params.id);
-  const book = books.find(
-    (book) => book.id === id
-  );
+  const book = books.find((book) => book.id === id);
   if (book) res.send(book);
   else res.send(`${id} not found`);
 });
@@ -39,6 +37,31 @@ app.post("/book", (req, res) => {
   res.send("your book data saved successfully");
 });
 
+app.put("/book/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const index = books.findIndex((book) => book.id === id);
+  if(index!=-1){
+    books[index]={
+      id:id,
+      title:req.body.title,
+      author:req.body.author
+    }
+    res.send(`Book with id: ${id} replced successfully`);
+  }
+  else
+    res.status(404).send(`Book with id: ${id} not found`);
+});
+app.patch("/book/:id",(req,res)=>{
+const id=parseInt(req.params.id);
+const book=books.find(book=>book.id===id);
+if(book){
+if(req.body.title) book.title=req.body.title
+if(req.body.author) book.author=req.body.author
+res.send(`Book with id: ${id} updated successfully`);
+}
+else
+  res.status(404).send(`Book with id: ${id} not found`)
+})
 app.listen(1234, () => {
   console.log("listen on 1234 port");
 });
